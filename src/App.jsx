f709ffd436de0726e5435f04b2227320dff51b41
Jeremy <the.jeremy.cobb@gmail.com>
2023-10-30 13:06:16 -0500

    added navbar code to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Navbar from './component/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 
 const App = () => {
     return(
         <BrowserRouter>
+            <Navbar />
             <Routes>
                 <Route to="/" element={<Home />}>Home</Route>
                 <Route to="/about" element={<About />}>About</Route>
