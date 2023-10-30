bb4d52c2095b3b01a0b37dc31c0b9cc70fa6d4b1
Jeremy <the.jeremy.cobb@gmail.com>
2023-10-30 13:04:30 -0500

    added navbar component in component folder

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return(
+        <div>
+            <Link path="/">Home</Link>
+            <Link path="/about">About</Link>
+        </div>
+    );
+}
